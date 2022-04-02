f9f9e3089c8c2937cb0ca765cf521f785b6762c2
BRB3D <samuraix221@hotmail.com>
2022-04-02 13:52:14 -0400

    Added Double, Float, Char, Boolean , and byte variables.

--- src/Application.java
+++ src/Application.java
@@ -12,5 +12,20 @@ public class Application {
 //        For longer values you can use long.
         long myLong = 9797;
         System.out.println(myLong);
+//        For extra precision you can use double.
+        double myDouble = 7.3243;
+        System.out.println(myDouble);
+//        Float for smaller decimal numbers you have to put an f at the end.
+        float myFloat = 324.3f;
+        System.out.println(myFloat);
+//        Char type can hold a unicode character and specify it by putting it in single quotes.
+        char myChar = 'y';
+        System.out.println(myChar);
+//        Boolean types can only hold true or false.
+        boolean myBoolean = true;
+        System.out.println(myBoolean);
+//        Byte can only hold 8 bits of data which is a number negative or positive that can go to about 127.
+        byte myByte = 127;
+        System.out.println(myByte);
     }
 }
