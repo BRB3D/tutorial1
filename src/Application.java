f5951b6d77815bfd234fa15474aea64110a130c5
BRB3D <samuraix221@hotmail.com>
2022-04-02 13:45:14 -0400

    Number variables long short int.

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,16 @@
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int myNumber;
+        myNumber = 80;
+        System.out.println(myNumber);
+        int mySecondNumber = 89;
+        System.out.println(mySecondNumber);
+//        For values that are smaller you can use 16 bit called short.
+        short myShort = 847;
+        System.out.println(myShort);
+//        For longer values you can use long.
+        long myLong = 9797;
+        System.out.println(myLong);
     }
 }
